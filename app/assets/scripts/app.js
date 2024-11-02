0f0866175b46f931acfceeb42e8a87fcafb37173
Jordan Kasper <jordan@jordankasper.com>
2024-11-02 11:18:50 -0400

    fixed rule build in app; fixed internal rule links

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -9,6 +9,7 @@ import initLoadScenario from './loadScenario.js'
 function main(page) {
     console.info(`Loading page: ${page}`)
     if (PAGE_INIT[page]) { PAGE_INIT[page]() }
+    if (/\/rules/.test(window.location.href)) { switchRuleLinks() }
 
     addSharedControls(page)
 }
@@ -32,6 +33,22 @@ function addSharedControls(page) {
         const selector = e.target.getAttribute('data-modal')
         if (selector) {
             $(`.${selector}`).attr('open', '')
+        }
+    })
+}
+
+function switchRuleLinks() {
+    $('a').forEach((link) => {
+        let href = link.getAttribute('href')
+        if (!/^http/.test(href) && /\.md/.test(href)) {
+            console.log(`switching link to html (${href})`)
+            href = href.replace('.md', '.html')
+            link.setAttribute('href', href)
+        } else if (/^\.\.\/scenarios/.test(href)) {
+            console.log(`switching link to scenarios in app (${href})`)
+            link.setAttribute('href', '/load-scenario')
+        } else {
+            console.log(`leaving link as is: ${href}`)
         }
     })
 }
